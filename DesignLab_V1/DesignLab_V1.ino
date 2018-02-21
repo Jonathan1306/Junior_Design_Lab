17e4d0a77d5c81f0216ea3d5abf9e6de592150d2
dwalsh74 <dfw035@mun.ca>
2018-02-21 17:13:25 -0330

    Finished Temperature Reading and Display

--- DesignLab_V1/DesignLab_V1.ino
+++ DesignLab_V1/DesignLab_V1.ino
@@ -1,97 +1,58 @@
-// Demo of Grove - Starter Kit V2.0
 
-// Reads the value of the Grove - Temperature Sensor, converts it to a Celsius temperature,
-// and prints it to the serial console.
-// Connect the Grove - Temperature Sensor to the socket marked A0
-// Open the Serial Monitor in the Arduino IDE after uploading
+//Design Lab Code
 
-// Define the pin to which the temperature sensor is connected.
-const int pinTemp = A0;
-
-// Define the B-value of the thermistor.
-// This value is a property of the thermistor used in the Grove - Temperature Sensor,
-// and used to convert from the analog value it measures and a temperature value.
-const int B = 4250;
-
-void setup()
-{
-    // Configure the serial communication line at 9600 baud (bits per second.)
-    Serial.begin(9600);
-}
-
-void loop()
-{
-    // Get the (raw) value of the temperature sensor.
-    int val = analogRead(pinTemp);
-
-    // Determine the current resistance of the thermistor based on the sensor value.
-    float resistance = (float)(1023-val)*10000/val;
-
-    // Calculate the temperature based on the resistance value.
-    float temperature = 1/(log(resistance/10000)/B+1/298.15)-273.15;
-
-    // Print the temperature to the serial console.
-    Serial.println(temperature);
-
-    // Wait one second between measurements.
-    delay(1000);
-}
-
-/*
-  SerialDisplay.ino
-  2013 Copyright (c) Seeed Technology Inc.  All right reserved.
-
-  Author:Loovee
-  2013-9-18
-
-  Grove - Serial LCD RGB Backlight demo.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+/*Done so far:
+  -  Reads the value of two Grove - Temperature Sensor, converts them to Celsius temperatures,
+     and prints them to the LCD screen. The sensors are cnnected to pins A0 and A1.
 */
-/*
+
+//Includes
 #include <Wire.h>
 #include "rgb_lcd.h"
 
+// Define the pin to which the temperature sensor is connected.
+const int pinTemp = A0;
+const int pin2Temp = A1;
+
+// Define the B-value of the thermistors.
+// This value is a property of the thermistor used in the Grove - Temperature Sensor,
+// and used to convert from the analog value it measures and a temperature value.
+const int B = 4275;
+const int R0 = 100000; //100k resistor
+
 rgb_lcd lcd;
 
-void setup(){
+void setup()
+{
     // set up the LCD's number of columns and rows:
     lcd.begin(16, 2);
-    // initialize the serial communications:
-    Serial.begin(9600);
 }
 
 void loop()
 {
-    // when characters arrive over the serial port...
-    if (Serial.available()) 
-    {
-        // wait a bit for the entire message to arrive
-        delay(100);
-        // clear the screen
-        lcd.clear();
-        // read all the available characters
-        while (Serial.available() > 0) 
-        {
-            // display each character to the LCD
-            lcd.write(Serial.read());
-        }
-    }
-}
+    // Get the (raw) value of the temperature sensors.
+    int val = analogRead(pinTemp);
+    int val2 = analogRead(pin2Temp);
 
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
+    // Determine the current resistance of the thermistors based on the sensor values.
+    float resistance = (float)(1023-val)*R0/val;
+    float resistance2 = (float)(1023-val2)*R0/val2;
+
+    // Calculate the temperatures based on the resistance values.
+    float temperature = 1/(log(resistance/R0)/B+1/298.15)-273.15;
+    float temperature2 = 1/(log(resistance2/R0)/B+1/298.15)-273.15;
+
+    // clear the screen
+    lcd.clear();
+    
+    //Output the Temperatures to the LCD
+    lcd.print("FRIDGE: ");
+    lcd.print(temperature);
+    lcd.setCursor(0, 1);
+    lcd.print("FREEZER: ");
+    lcd.print(temperature2);
+    
+    // Wait one second between measurements.
+    delay(1000);
+}
 

7e842a4f37acf2876f96b39135ac8127e1f382a4
dwalsh74 <dfw035@mun.ca>
2018-04-03 12:11:02 -0230

    Final Revision (Probably)
    It's definitely most likely maybe probably done for real this time

--- DesignLab_V2/DesignLab_V2/DesignLab_V2.ino
+++ DesignLab_V2/DesignLab_V2/DesignLab_V2.ino
@@ -1,6 +1,7 @@
 
 
-/**********************************
+/***************************************
+ * 
  * Engineering 5800 
  * Junior Design Project
  * 
@@ -11,32 +12,32 @@
  * Jonathan Traverse - 201411451
  * David Walsh - 201435609
  * 
-**********************************/
+***************************************/
 
-//Includes
+// Includes
 #include <Wire.h>
 #include "rgb_lcd.h"
 #include <SPI.h>
 #include <SD.h>
 #include <stdlib.h>
 
-//Temperature sensor pins
+// Temperature sensor pins
 const int pinTemp = A0;
 const int pin2Temp = A1;
 
-//Push button pin
+// Push button pin
 const int buttonPin = 2;
 
-//Current sensor pin
+// Current sensor pin
 const int sensorIn = A2;
 
-//Start/Stop Recording LED pin
+// Start/Stop Recording LED pin
 const int startLED = 3;
 
-//SD Eject LED pin
+// SD Eject LED pin
 const int ejectLED = 4;
 
-//ACS712 sensitivity
+// ACS712 sensitivity
 const int sensitivity = 175;
 
 //ACS712 Output Voltage
@@ -45,17 +46,14 @@ double Voltage = 0;
 //ACS712 Output RMS Voltage
 double VRMS = 0;
 
-//DACS712 Output RMS Current
+// DACS712 Output RMS Current
 double AmpsRMS = 0;
 
-//Load power consumption
+// Load power consumption
 double Power = 0;
 
-//Wall output voltage for power calculation.
+// Wall output voltage for power calculation.
 double Vwall = 117.0;
-
-//Reference voltage of the ACS712
-//float Vref = 2500;
 
 // Define the B-value of the thermistors.
 // This value is a property of the thermistor used in the Grove - Temperature Sensor,
@@ -65,32 +63,38 @@ const int R0 = 100000; //100k resistor
 
 rgb_lcd lcd;
 
+ /*********************************************
+  * 
+ * Set up the various pins and components
+ * required by this proect
+ * 
+*********************************************/
 void setup()
 {
-    // set up the LCD's number of columns and rows:
+    // Set up the LCD and specify dimensions
     lcd.begin(16, 2);
-    Serial.begin(9600);//TEMP
+    Serial.begin(9600);
 
-    //set up button
+    // Set up button
     pinMode(buttonPin, INPUT);
     
-    //set up LEDs
+    // Set up LEDs
     pinMode(startLED, OUTPUT);
     pinMode(ejectLED, OUTPUT);
 
-    // see if the card is present and can be initialized:
+    // See if the card is present and can be initialized:
     if (!SD.begin(4)) 
     {
-      //Display error message
+      // Display error message
       lcd.print("SD CARD ERROR");
       lcd.setCursor(0, 1);
       lcd.print("CARD NOT FOUND");
 
-      //Toggle LEDs
+      // Toggle LEDs
       digitalWrite(startLED,LOW);
       digitalWrite(ejectLED,HIGH);
       
-      //Wait for Button Push to try again
+      // Wait for Button Push to try again
       int i = 0;
       while (i == 0)
       {
@@ -108,19 +112,31 @@ void setup()
 
 void loop()
 {
-  //Enter Idle State on Startup
+  // Enter Idle State on Startup
   idleState();   
 }
 
+
+/*********************************************
+ * Handles the system's Idle State  
+ * 
+ * In this state:
+ *  - No recordings are taken
+ *  - It is safe to eject the SD card
+ *  - The system will remain in this state
+ *    until it recieves user input
+ *  
+*********************************************/
+
 void idleState()
 {   
-  //Change LCD Output
+  // Change LCD output
   lcd.clear();
   lcd.print("IDLE STATE");
   lcd.setCursor(0, 1);
   lcd.print("PUSH TO START");
   
-  //Toggle LEDs
+  // Toggle LEDs
   digitalWrite(startLED,LOW);
   digitalWrite(ejectLED,HIGH);  
   
@@ -128,15 +144,34 @@ void idleState()
   delay(1000);
   while (!digitalRead(buttonPin))
   {
-    //Do nothing 
+    // Do nothing 
   }
   
-  //Enter Recording State
+  // Enter Recording State
   delay(1000);
   recordingState();
-  
-  //**Maybe add jump to step 1 in case gets past this 
 }
+
+
+/*********************************************
+ * 
+ * Handles the system's Recording State  
+ * 
+ * In this state:
+ * 
+ *  - Recordings are regularly taken
+ *  
+ *  - Real time tempratures and power consumtion
+ *    are displayed on the LCD screen
+ *    
+ *  - Data is regularly written to the SD card
+ *  
+ *  - It is not safe to eject the SD card
+ *  
+ *  - The system will remain in this state
+ *    until it recieves user input
+ *  
+*********************************************/
 
 void recordingState()
 {  
@@ -153,42 +188,41 @@ void recordingState()
     // String for assembling the data to log:
     String dataString = "";
     
-    //Buffer for temporary strings
+    // Buffer for temporary strings
     String buff = "";
     
     // Get the (raw) value of the temperature sensors.
     int val = analogRead(pinTemp);
     int val2 = analogRead(pin2Temp);
 
-    // Determine the current resistance of the thermistors based on the sensor values.
+    // Determine resistance of the thermistors based on the sensor values.
     float resistance = (float)(1023.0-val)*R0/val;
     float resistance2 = (float)(1023.0-val2)*R0/val2;
 
-    // Calculate the temperatures based on the resistance values and append to log string.
+    // Calculate temperature values and append to log string.
     float temperature = 1/(log(resistance/R0)/B+1/298.15)-273.15;
     
     dataString += String(val); //Raw Data
-    //Serial.print(temperature);
     dataString += String(",");
     
     float temperature2 = 1/(log(resistance2/R0)/B+1/298.15)-273.15;
     dataString += String(val2); //Raw Data
     dataString += String(",");
 
-    //CURRENT SENSOR//
+    // CURRENT SENSOR
     Voltage = getVPP();
     VRMS = (Voltage/2.0) *0.707; 
     AmpsRMS = (VRMS * 1000)/sensitivity;
     Power = AmpsRMS * Vwall;
     
-    //Print Values DEBUGGING w/serial monitor only
+    // Print Values DEBUGGING w/serial monitor only
     Serial.print(AmpsRMS);
     Serial.println(" Amps RMS");
 
-    //Append to log string
+    // Append to log string
     dataString += String(sensorIn);
     
-    //Log to SD Card
+    // Log to SD Card
     File dataFile = SD.open("datalog.txt", FILE_WRITE);
 
     // If the file is available, write to it
@@ -208,11 +242,11 @@ void recordingState()
       lcd.setCursor(0, 1);
       lcd.print("CHECK SD CARD");
       
-      //Toggle LEDs
+      // Toggle LEDs
       digitalWrite(startLED,LOW);
       digitalWrite(ejectLED,HIGH);
       
-      //Wait for Button Push to try again
+      // Wait for Button Push to try again
       int i = 0;
       while (0 == i)
       {
@@ -230,7 +264,7 @@ void recordingState()
 
     // Clear the screen to display temperature stats
     lcd.clear();
-    //Output the Temperatures to the LCD
+    // Output the Temperatures to the LCD
     lcd.print("FRIDGE: ");
     lcd.print(temperature);
     lcd.print("C");
@@ -239,7 +273,7 @@ void recordingState()
     lcd.print(temperature2);
     lcd.print("C");
     
-    // Wait 2.5 second between screen swiches
+    // Wait 1.25 second between screen swiches
     delay(1250); 
 
     if(digitalRead(buttonPin))
@@ -247,50 +281,64 @@ void recordingState()
       continue;
     }
         
-    //Clear the screen again to display current and power stats.
+    // Clear the screen again to display current and power stats.
     lcd.clear();
-    //Output the Current and Power to the LCD
+    // Output the Current and Power to the LCD
     lcd.print("CURRENT: ");
     lcd.print(AmpsRMS);
     lcd.print("A");
     lcd.setCursor(0, 1);
-    lcd.print("Power: ");
+    lcd.print("POWER: ");
     lcd.print(Power);
     lcd.print("W");
 
-    //Wait 2.5 second between measurements.
+    // Wait 1.25 second between measurements.
     delay(1250);
     
   }
   idleState();
 }
+
+
+ /*********************************************
+ * 
+ * Retrieve pk-pk voltage
+ * 
+ * This function is used on to calculate a 
+ * voltage value using the current sensor's 
+ * raw data.
+ *  
+*********************************************/
+
  float getVPP()
 {
   float result;
-  
-  int readValue;             //value read from the sensor
-  int maxValue = 0;          // store max value here
-  int minValue = 1023;          // store min value here
+
+  // Read from the current sensor
+  int rawValue;             
+  int maxValue = 0;       
+  int minValue = 1023;
   
    uint32_t start_time = millis();
-   while((millis()-start_time) < 1000) //sample for 1 Sec
+   //Continuously sample the sensor for one second
+   while((millis()-start_time) < 1000)
    {
-       readValue = analogRead(sensorIn);
-       // see if you have a new maxValue
-       if (readValue > maxValue) 
+       rawValue = analogRead(sensorIn);
+       // Check for a new maximum value
+       if (rawValue > maxValue) 
        {
-           /*record the maximum sensor value*/
-           maxValue = readValue;
+           maxValue = rawValue;
        }
-       if (readValue < minValue) 
+       // Check for a new minimum value
+       if (rawValue < minValue) 
        {                                                                                                                                                                                                            
-           /*record the maximum sensor value*/
-           minValue = readValue;
+           minValue = rawValue;
        }
    }
    
-   // Subtract min from max
+   // Subtract min value from max value
    result = ((maxValue - minValue) * 5.0)/1023.0;
      
+   //Return pk-pk voltage
    return result;
  }
